f718277f13723a51cc12e449715990ad328fc47d
Harry Kerans <harrykerans@outlook.com>
2025-08-21 15:47:52 +0100

    Working TCP connection and byte stream reader

---
+++ cmd/tcplistener/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+
+	listener, listenError := net.Listen("tcp4", "127.0.0.1:42069")
+	if listenError != nil {
+		log.Fatal(listenError)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		fmt.Println("Connection Accepted From", conn.RemoteAddr())
+		if err != nil {
+			log.Fatal(err)
+		}
+		go func(c net.Conn) {
+			linesChan := getLineslinesannel(c)
+			for line := range linesChan {
+				fmt.Println(line)
+			}
+			c.Close()
+			fmt.Println("Connection to ", conn.RemoteAddr(), " Closed!")
+		}(conn)
+
+	}
+
+}
+
+func getLineslinesannel(tcpLine io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer tcpLine.Close()
+		defer close(lines)
+		currentLine := ""
+		for {
+			buffer := make([]byte, 8)
+			n, err := tcpLine.Read(buffer)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					if currentLine != "" {
+						lines <- currentLine
+						currentLine = ""
+					}
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				break
+			}
+			linesunk := buffer[:n]
+			parts := bytes.Split(linesunk, []byte("\n"))
+
+			currentLine += string(parts[0])
+			if len(parts) != 1 {
+				lines <- currentLine
+				currentLine = string(parts[len(parts)-1])
+			}
+		}
+	}()
+	return lines
+}
