2ea14ee86124f4b3bbdd4ab49032572c854bf7ae
Harry Kerans <harrykerans@outlook.com>
2025-08-22 13:59:19 +0100

    added invalid version test

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -40,4 +40,8 @@ func TestRequestLineParse(t *testing.T) {
 	// Test: Invalid method
 	_, err = RequestFromReader(strings.NewReader("POSTYY /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+
+	// Test: Invalid version
+	_, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/2\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
 }
